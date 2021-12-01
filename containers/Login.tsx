c69633c3fa461503370d7356b5a56e303c7ec076
Logon Aluno <logonrmlocal@fiap.com.br>
2021-11-30 23:13:40 -0300

    Login Style

---
+++ containers/Login.tsx
@@ -0,0 +1,18 @@
+
+export const Login = () => {
+    return (
+        <div className="container-login">
+            <img src="/logo.svg" className="logo" alt="Logo Fiap">
+            </img>
+            <div className="input">
+                <img src="/mail.svg" alt="Logo Fiap"></img>
+                <input></input>
+            </div>
+            <div className="input">
+                <img src="/lock.svg" alt="Logo Fiap"></img>
+                <input></input>
+            </div>
+            <button>Login</button>
+        </div>
+    )
+}
