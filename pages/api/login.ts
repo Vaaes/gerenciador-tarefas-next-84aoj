7b73146f9d763cc1a635567aa32ff036a259f7bb
Logon Aluno <logonrmlocal@fiap.com.br>
2021-12-02 20:56:56 -0300

    Front Login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,31 +5,31 @@ import { UserModel } from '../../models/UserModel';
 import { DefaultResponseMsg } from '../../types/DefaultResponseMsg';
 import { LoginRequest } from '../../types/LoginRequest';
 import jwt from 'jsonwebtoken';
+import { LoginResponse } from '../../types/LoginResponse';
 
-const LoginEndpoint =  async(req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg | LoginResponse>) => {
+const loginEndpoint = async(req : NextApiRequest, 
+    res : NextApiResponse<DefaultResponseMsg | LoginResponse>) => {
 
     const {MY_SECRET_KEY} = process.env;
     if(!MY_SECRET_KEY){
-        return res.status(500).json({ error: "ENV chave JWT não informou" })
+        return res.status(500).json({ error : "ENV Chave JWT não informada"});
     }
-
+    
 
     if(req.method === 'POST'){
-        const body = req.body as LoginRequest;
-
-        if(!body || !body.login || !body.password)
-        {
-            return res.status(400).json({ error : 'Favor informar usuario e senha'});
+        const body = JSON.parse(req.body) as LoginRequest;
+        if(!body || !body.login || !body.password){
+            return res.status(400).json({ error : 'Favor informar usuário e senha'});
         }
-        
-        const userFound = await UserModel.find({ email : body.login, password : md5(body.password) })
-        if(userFound && userFound.length > 0){
-            const user = userFound[0];
-            const token = jwt.sign({ _id : user._id }, MY_SECRET_KEY)
 
+        const usersFound = await UserModel.find({ email : body.login, password : md5(body.password)});
+        if( usersFound && usersFound.length > 0 ){
+            const user = usersFound[0];
+            const token = jwt.sign({_id : user._id}, MY_SECRET_KEY);
+            
             const result = {
                 name : user.name,
-                email: user.email,
+                email : user.email,
                 token
             }
 
@@ -42,4 +42,4 @@ const LoginEndpoint =  async(req : NextApiRequest, res : NextApiResponse<Default
     return res.status(405).json({ error : 'Metodo infomado não é valido'});
 }
 
-export default connectDb(LoginEndpoint)
+export default connectDb(loginEndpoint);
